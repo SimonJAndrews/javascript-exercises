e21f38ce60c44eccdbbb7b86134d6fdc13dff20f
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-08 19:09:29 +0100

    hi

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,6 @@ const reverseString = function(str) {
 return str.split("").reverse().join("");
 };
 
-reverseString("hello");
+reverseString("hello there");
 // Do not edit below this line
 module.exports = reverseString;
