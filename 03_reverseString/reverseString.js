5c11b079cd9c61decaa3846291f01c94677cb06c
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-08 18:50:01 +0100

    adding solition to reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function() {
-
+let stringInput = 'National Geographic Magazine';
+let reversal = stringInput.split('').reverse().join();
+return reversal;
 };
 
 // Do not edit below this line
