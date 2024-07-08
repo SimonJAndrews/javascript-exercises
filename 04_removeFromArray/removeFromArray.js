60f95702d449b90d0c2706ccce1b0e17749d9452
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-08 21:38:06 +0100

    hi

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function() {
     const valuesArr = [1,2,3,4],
   removeValFromIndex = [1,2];
 for (const i of removeValFromIndex.reverse()) {
-  valuesArr.splice(i, 1);
+  valuesArr.splice(i, 1);;
   return valuesArr
   
 }}
