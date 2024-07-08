a737c66a2fc954b2ec13ed3154e399bb2f67e481
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-08 20:50:01 +0100

    hi

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
 const removeFromArray = function() {
-let myArray = [1,2,3,4]
- return myArray.splice(1,2)
-
-};
+    let myArray = [1,2,3,4]
+      myArray.splice(2,1);
+      return myArray;
+    
+    };
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
