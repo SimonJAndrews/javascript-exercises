79feb7febcf65c60f6085838acb3d094edf0ceaf
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-11 19:09:05 +0100

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(array, ...numbersToRemove) {
     return array.filter(item => !numbersToRemove.includes(item))
   };
   
-removeFromArray([1, 2, 3], "1", 3).toEqual([1, 2]);
+removeFromArray([1, 2, 3], "1", 3);
 
 
   
