0ca0cdc64d6b06b5bd9c33005080062b6a5efb8c
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-12 19:39:42 +0100

    test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,12 @@
-let array = [1,2,3,4,5]
+let array = [1,2,3,4]
 let sum = 0;
-const sumAll = function(sum) {
-for(i = 0; i < array.length; i++){
-sum += array[i]
 
-return sum
-}};
+function sumAll(){
+for(let i = 0 ; i < array.length;i++){
+sum += array[i];
+}
+return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
